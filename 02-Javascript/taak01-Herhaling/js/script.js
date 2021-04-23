30a077c8fd6cc00624669df279021d05aad0301a
Hayder Abidi <hayder.abidi@student.rocva.nl>
2021-04-23 11:00:06 +0100

    js1

--- 02-Javascript/taak01-Herhaling/js/script.js
+++ 02-Javascript/taak01-Herhaling/js/script.js
@@ -1,2 +1,19 @@
 let strNaam;
 let boolNaamKlopt;
+strNaam = prompt("Wat is je naam?");
+
+if(boolNaamKlopt = confirm("Heet je echt " + strNaam + "?")){
+    alert("Welkom " + strNaam + "!");
+}
+else{
+    strNaam = prompt("Nogmaals, hoe heet je?");
+}
+if(boolNaamKlopt = confirm("Heet je echt " + strNaam + "?")){
+    alert("Welkom " + strNaam + "!");
+}
+else{
+    alert("Evengoed, welkom!"); 
+}
+
+
+console.log(strNaam);
