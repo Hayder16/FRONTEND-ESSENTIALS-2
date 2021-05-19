d2d549749a21b41ba3c0bc3c3fa68269dc721b7b
Hayder Abidi <hayder.abidi@student.rocva.nl>
2021-05-19 11:51:03 +0100

    parameter

---
+++ 02-Javascript/taak04-Functions-Parameters/js/script.js
@@ -0,0 +1,9 @@
+let getal1 = prompt('Kies een getal', 10);
+let getal2 = prompt('Kies nog een getal', 8);
+
+function sum(getal1, getal2) {
+    let som = getal1 * getal2;
+    return som;
+}
+
+console.log(sum(10,8))
