eb729503cad0e0d6f62f7824ed0f0c455bd08be0
Hayder Abidi <hayder.abidi@student.rocva.nl>
2021-05-21 11:34:22 +0100

    Array uitlezen

--- 02-Javascript/taak08-Arrays-Uitlezen/js/script.js
+++ 02-Javascript/taak08-Arrays-Uitlezen/js/script.js
@@ -11,6 +11,9 @@ topFilms = [
     "Lawrence of Arabia"
 ];
 
+
+
+
 document.querySelector(".film1-ranking").innerHTML = 1;
 document.querySelector(".film1-naam").innerHTML = topFilms[0];
 document.querySelector(".film2-ranking").innerHTML = 2;
@@ -32,4 +35,58 @@ document.querySelector(".film9-naam").innerHTML = topFilms[8];
 document.querySelector(".film10-ranking").innerHTML = 10;
 document.querySelector(".film10-naam").innerHTML = topFilms[9];
 
+function kiesFavorieteFilm(){
+    answer = prompt('Wat is de nummer van je favoriete film?');
+    if(answer < 0) {
+        alert('Dit getal is te klein');
+        return false;
+    } else if(answer > 10) {
+        alert('Dit getal is te groot');
+        return false;
+    } else if(answer === null) {
+        return false;
+    }
+    if(answer == 1) {
+        console.log(alert = 'The Godfather')
+        document.querySelector(".film1-naam").style.backgroundColor = "Blue";
+        document.querySelector(".film1-naam").style.color = 'White';
+    } else if(answer == 2) {
+        console.log(alert = 'The Shawshank Redemption')
+        document.querySelector(".film2-naam").style.backgroundColor = "Blue";
+        document.querySelector(".film2-naam").style.color = 'White';
+    } else if(answer == 3) {
+        console.log(alert = 'Schindlers List')
+        document.querySelector(".film3-naam").style.backgroundColor = "Blue";
+        document.querySelector(".film3-naam").style.color = 'White';
+    } else if(answer == 4) {
+        console.log(alert = 'Raging Bull')
+        document.querySelector(".film4-naam").style.backgroundColor = "Blue";
+        document.querySelector(".film4-naam").style.color = 'White';
+    } else if(answer == 5) {
+        console.log(alert = 'Casablanca')
+        document.querySelector(".film5-naam").style.backgroundColor = "Blue";
+        document.querySelector(".film5-naam").style.color = 'White';
+    } else if(answer == 6) {
+        console.log(alert = 'Citizen Kane')
+        document.querySelector(".film6-naam").style.backgroundColor = "Blue";
+        document.querySelector(".film6-naam").style.color = 'White';
+    } else if(answer == 7) {
+        console.log(alert = 'Gone with the Wind')
+        document.querySelector(".film7-naam").style.backgroundColor = "Blue";
+        document.querySelector(".film7-naam").style.color = 'White';
+    } else if(answer == 8) {
+        console.log(alert = 'The Wizard of Oz')
+        document.querySelector(".film8-naam").style.backgroundColor = "Blue";
+        document.querySelector(".film8-naam").style.color = 'White';
+    } else if(answer == 9) {
+        console.log(alert = 'One Flew Over the Cuckoos Nest')
+        document.querySelector(".film9-naam").style.backgroundColor = "Blue";
+        document.querySelector(".film9-naam").style.color = 'White';
+    } else if(answer == 10) {
+        console.log(alert = 'Lawrence of Arabia')
+        document.querySelector(".film10-naam").style.backgroundColor = "Blue";
+        document.querySelector(".film10-naam").style.color = 'White';
+    }
+}
 
+kiesFavorieteFilm();
