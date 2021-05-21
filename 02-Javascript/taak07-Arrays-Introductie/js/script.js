6ae0fe5b00a19a30b4b42787c24d437b44c53ee0
Hayder Abidi <hayder.abidi@student.rocva.nl>
2021-05-21 10:43:57 +0100

    array

---
+++ 02-Javascript/taak07-Arrays-Introductie/js/script.js
@@ -0,0 +1,13 @@
+topFilm = ["Coah Carter", "Bad boys", "Maze Runner"];
+topGames = ["Call of duty", "Fifa", "Rust"];
+topMuziek = ["Another day gone", "Mixed Emotions", "Justice"];
+
+console.log(topFilm[0])
+console.log(topFilm[1])
+console.log(topFilm[2])
+console.log(topGames[0])
+console.log(topGames[1])
+console.log(topGames[2])
+console.log(topMuziek[0])
+console.log(topMuziek[1])
+console.log(topMuziek[2])
