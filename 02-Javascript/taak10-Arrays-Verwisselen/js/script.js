f3c4989edaf2fcf63ceeb425b1d3612f64d70b03
Hayder Abidi <hayder.abidi@student.rocva.nl>
2021-05-28 11:46:29 +0100

    10

--- 02-Javascript/taak10-Arrays-Verwisselen/js/script.js
+++ 02-Javascript/taak10-Arrays-Verwisselen/js/script.js
@@ -18,13 +18,45 @@ document.querySelector(".film5-ranking").innerHTML = 5;
 document.querySelector(".film5-naam").innerHTML = topFilms[4];
 
 function moveup(nummer) {
-
+    if(nummer == 1) {
+        null;
+    } else if(nummer == 2) {
+        document.querySelector('.film1-naam').innerHTML = topFilms[1];
+        document.querySelector('.film2-naam').innerHTML = topFilms[0];
+    } else if(nummer == 3) {
+        document.querySelector('.film2-naam').innerHTML = topFilms[2];
+        document.querySelector('.film3-naam').innerHTML = topFilms[1];
+    } else if(nummer == 4) {
+        document.querySelector('.film3-naam').innerHTML = topFilms[3];
+        document.querySelector('.film4-naam').innerHTML = topFilms[2];
+    } else if(nummer == 5) {
+        document.querySelector('.film4-naam').innerHTML = topFilms[4];
+        document.querySelector('.film5-naam').innerHTML = topFilms[3];
+    }
 }
 
 function movedown(nummer) {
-
+    if(nummer == 1) {
+        document.querySelector('.film1-naam').innerHTML = topFilms[1];
+        document.querySelector('.film2-naam').innerHTML = topFilms[0];
+    } else if(nummer == 2) {
+        document.querySelector('.film2-naam').innerHTML = topFilms[2];
+        document.querySelector('.film3-naam').innerHTML = topFilms[1];
+    } else if(nummer == 3) {
+        document.querySelector('.film3-naam').innerHTML = topFilms[3];
+        document.querySelector('.film4-naam').innerHTML = topFilms[2];
+    } else if(nummer == 4) {
+        document.querySelector('.film4-naam').innerHTML = topFilms[4];
+        document.querySelector('.film5-naam').innerHTML = topFilms[3];
+    } else if(nummer == 5) {
+        null;
+    }
 }
 
 function resetList() {
-
+    document.querySelector('.film1-naam').innerHTML = topFilms[0];
+    document.querySelector('.film2-naam').innerHTML = topFilms[1];
+    document.querySelector('.film3-naam').innerHTML = topFilms[2];
+    document.querySelector('.film4-naam').innerHTML = topFilms[3];
+    document.querySelector('.film5-naam').innerHTML = topFilms[4];
 }
