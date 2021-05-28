9a340f7fc52f579d2cc9740115784d3693f9f6e8
Hayder Abidi <hayder.abidi@student.rocva.nl>
2021-05-28 10:11:12 +0100

    loops in loops

--- 02-Javascript/taak10-Arrays-Verwisselen/js/script.js
+++ 02-Javascript/taak10-Arrays-Verwisselen/js/script.js
@@ -17,7 +17,14 @@ document.querySelector(".film4-naam").innerHTML = topFilms[3];
 document.querySelector(".film5-ranking").innerHTML = 5;
 document.querySelector(".film5-naam").innerHTML = topFilms[4];
 
-function moveup() {
-    
+function moveup(nummer) {
+
 }
 
+function movedown(nummer) {
+
+}
+
+function resetList() {
+
+}
--- 02-Javascript/taak16-Loops-in-Loops/js/script.js
+++ 02-Javascript/taak16-Loops-in-Loops/js/script.js
@@ -1,8 +1,21 @@
 var arrProductPrijs = [5,10,20,50,100];
 var arrProductKorting = [0.95, 0.85, 0.98, 1.21];
 var totaal = 0;
-
+ 
 function rondaf(getal, aantalplekkenachterkomma){
     const x = Math.pow(10, aantalplekkenachterkomma);
     return Math.round(getal * x)/x;
 }
+var printTotaal=0;
+ 
+for (let i=0; i < arrProductPrijs.length; i++){
+ 
+    for(let i=0; i < arrProductKorting.length; i++){
+        arrProductPrijs[i] = arrProductPrijs[i] - arrProductKorting[i];
+        totaal = arrProductPrijs[i];
+        totaalPrijs = rondaf(totaal,2);
+    }
+    console.log("Product " + i + " : " +totaalPrijs);
+    printTotaal=totaalPrijs;
+}
+console.log("Totaal" + " : " + printTotaal);
